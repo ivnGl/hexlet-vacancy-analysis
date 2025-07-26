f9c1d0098848aa9ee29aee0333845df92d77fab9
DaniilShomin <d.sh.v@yandex.ru>
2025-07-26 14:01:35 +0300

    add PasswordReset model

--- app/services/auth/password_reset/models.py
+++ app/services/auth/password_reset/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from app.services.auth.users.models import User
+
+
+class PasswordReset(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    token_hash = models.CharField(255, null=True)
+    expires_at = models.DateTimeField()
+    is_used = models.BooleanField(default=False)
