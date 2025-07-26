bdf23ef70f3d75cf7b2e31fa2ac82b318bfef951
DaniilShomin <d.sh.v@yandex.ru>
2025-07-27 00:17:50 +0300

    update app name

--- app/services/auth/password_reset/apps.py
+++ app/services/auth/password_reset/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PasswordResetConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "password_reset"
+    name = "app.services.auth.password_reset"
