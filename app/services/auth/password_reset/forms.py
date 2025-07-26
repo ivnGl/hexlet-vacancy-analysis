c17442081cac9dc759f50de7b981d36018e99062
DaniilShomin <d.sh.v@yandex.ru>
2025-07-26 14:00:30 +0300

    update form: CustomPasswordResetForm

--- app/services/auth/password_reset/forms.py
+++ app/services/auth/password_reset/forms.py
@@ -1,7 +1,4 @@
-import logging
 from django.contrib.auth.forms import PasswordResetForm
-
-logger = logging.getLogger(__name__)
 
 
 class CustomPasswordResetForm(PasswordResetForm):
@@ -14,15 +11,3 @@ class CustomPasswordResetForm(PasswordResetForm):
         self.fields["email"].help_text = (
             "Enter the email address you provided during registration"
         )
-
-    def clean_email(self):
-        """
-        check if email belongs to user, if user does not exist - "error" instead of email
-        """
-        email = self.cleaned_data["email"].lower().strip()
-        user = self.get_users(email)
-        if not user:
-            logger.error(f"[{email}]: email does not belong to any user")
-            email = "error"
-
-        return email
