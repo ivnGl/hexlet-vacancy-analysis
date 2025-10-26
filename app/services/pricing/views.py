7513404aa947c84eef4539beb630356582c3b771
Lugovskikh Konstantin <kostya.lugovskikh@mail.ru>
2025-10-26 19:25:38 +0500

    update views

--- app/services/pricing/views.py
+++ app/services/pricing/views.py
@@ -1,3 +1,26 @@
-# from django.shortcuts import render
+from django.views import View
+from inertia import render as inertia_render
+from .models import PricingPlan
 
-# Create your views here.
+class PricingView(View):
+
+    def get(self, request):
+        plans = PricingPlan.objects.filter(is_active=True)
+        props = {
+            'plans': [
+                {
+                    'id': plan.id,
+                    'name': plan.name,
+                    'price': float(plan.price),
+                    'currency': plan.currency,
+                    'period': plan.period,
+                    'description': plan.description,
+                    'features': [
+                        {'name': feature.name, 'description': feature.description}
+                        for feature in plan.features.all()
+                    ]
+                }
+                for plan in plans
+            ]
+        }
+        return inertia_render(request, 'PricingPage, props')
