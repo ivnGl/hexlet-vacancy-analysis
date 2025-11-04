9a48479aada8d6c52dc4d9869e5720f6a2e944b7
Lugovskikh Konstantin <kostya.lugovskikh@mail.ru>
2025-11-04 18:33:16 +0500

    update model and add JSON file for frontend

--- app/services/pricing/models.py
+++ app/services/pricing/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class PricingPlan(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.PositiveIntegerField(default=0)
     currency = models.CharField(max_length=3, default='₽')
     period = models.CharField(max_length=50, default='мес')
     description = models.CharField(max_length=50)
