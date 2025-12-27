1b7b0b2abfa4f4047f9d8bd543011e99481bda82
ShominDaniil <d.sh.v@yandex.ru>
2025-12-28 02:38:06 +0300

    feat: create ChatMessage model

--- app/services/ai/models.py
+++ app/services/ai/models.py
@@ -1,3 +1,27 @@
-# from django.db import models
+from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from ..auth.users.models import User
+
+
+class ChatMessage(models.Model):
+    """
+    Модель для хранения сообщений в диалоге
+    """
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name="Пользователь",
+    )
+    messages = models.JSONField(blank=True, verbose_name="Сообщения")
+    cookie_hash = models.CharField(null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+
+    class Meta:
+        ordering = ("created_at",)
+        verbose_name = "Сообщение чата"
+        verbose_name_plural = "Сообщения чата"
