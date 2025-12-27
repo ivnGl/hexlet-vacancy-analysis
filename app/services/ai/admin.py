123a1e40f558f15c4420a516f71c46ca5f265a02
ShominDaniil <d.sh.v@yandex.ru>
2025-12-28 02:44:12 +0300

    feat: create ChatMessageAdmin

--- app/services/ai/admin.py
+++ app/services/ai/admin.py
@@ -1,3 +1,17 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import ChatMessage
+
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = ("user_display", "messages", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at")
+
+    def user_display(self, obj):
+        if obj.user:
+            return obj.user.username
+        else:
+            return "Аноним"
+
+    user_display.short_description = "Пользователь"  # type: ignore
--- app/services/ai/apps.py
+++ app/services/ai/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app.services.ai"
+    verbose_name = "AI чат"
